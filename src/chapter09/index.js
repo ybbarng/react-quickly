932760177360d4bcbd8c047a5c840b8827db1b29
ybbarng <ybbarng@gmail.com>
2018-09-02 00:05:23 +0900

    Add router for chapter 09

---
+++ src/chapter09/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Chapter09 extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Quickly - Chapter 09. Menu Component</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Chapter09;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Chapter09 from './chapter09';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div className="route-wrapper">
+      <Route exact path="/" component={App} />
+      <Route exact path="/chapter09" component={Chapter09} />
+    </div>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
