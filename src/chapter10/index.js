766d9d7275cc35ab7af09cafa3b4ab63f090545f
ybbarng <ybbarng@gmail.com>
2018-09-02 02:45:13 +0900

    Implement chapter 10

--- src/chapter10/index.js
+++ src/chapter10/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Tooltip from './Tooltip';
 
 class Chapter10 extends Component {
   render() {
     return (
       <div className="App">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
         <header className="App-header">
           <h1 className="App-title">React Quickly - Chapter 10. Tooltip Component</h1>
         </header>
+        <Tooltip text="The book you're reading now">React Quickly</Tooltip> was published in 2017. It's awesome!
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
       </div>
     );
   }
